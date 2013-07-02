ff407a2bcd159da840ab30519819d8e005fca192
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-07-02 11:38:05 -0500

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('*', function(req, res){
+  res.redirect('http://tumblr.com');
+});
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
