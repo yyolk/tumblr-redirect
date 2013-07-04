5517583074b663115a6ecd50823ee4e2b2b8e152
Joseph Yølk Chiocchi <joe@yolk.cc>
2013-07-04 05:22:10 -0500

    keen.io

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ var express = require('express')
 
 var app = express();
 
+
+var keenIO = require('keen.io');
+
+// Configure instance. Only projectId and writeKey are required to send data.
+var keen = keenIO.configure({
+    projectId: process.env['KEEN_PROJECT_ID'],
+    writeKey: process.env['KEEN_WRITE_KEY']
+});
+
+
+
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 
@@ -18,6 +30,13 @@ if ('development' == app.get('env')) {
 
 app.get('*', function(req, res){
   res.redirect('http://tumblr.com/dashboard');
+  keen.addEvent("applaunch", {"count": 1}, function(err, res) {
+    if(err){
+      consoloe.log("keen error");
+    } else {
+      console.log("keen success");
+    }
+});
 });
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
